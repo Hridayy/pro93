c72a4e7ec007bbb8766644892da1e40bea2bd382
Hridayy <73392185+Hridayy@users.noreply.github.com>
2021-06-29 13:47:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function addUser()
-{
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDIGugLwMGtp-WQd1Eval6vLIVGUkqxZPA",
+    authDomain: "kwitter-4dd9e.firebaseapp.com",
+    databaseURL: "https://kwitter-4dd9e-default-rtdb.firebaseio.com",
+    projectId: "kwitter-4dd9e",
+    storageBucket: "kwitter-4dd9e.appspot.com",
+    messagingSenderId: "915651673656",
+    appId: "1:915651673656:web:c947e4d2826743f75af0cd"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  function addUser() 
+  {
+  
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update
+        (
+            {
+            purpose: "adding user",
+            reason: "new user"
+            }
+        );
+ 
+
 user_name=document.getElementById("user_name").value;
 if(user_name !="")
 {
 localStorage.setItem("user_name",user_name);
-window.location="kwitter_room.html";
+window.location="twitter_room.html";
 }
 else{
     window.alert("please enter a username")
